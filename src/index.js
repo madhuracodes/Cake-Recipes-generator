086df4ae1aa8970bfda43e5769d44cceadb250f2
madhuracodes <madhura84@gmail.com>
2024-10-28 11:03:44 +0100

    added js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 alert("hello");
+function generateRecipe(event) {
+  event.preventDefault();
+
+  new Typewriter("#recipe", {
+    strings: "your cake recipe will be displayed here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
